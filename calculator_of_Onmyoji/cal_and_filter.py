cd8cd9d81c8841629df17faafae5cab31ce750ab
JiaJunsu <jiajunsu@huawei.com>
2018-06-04 10:16:26 -0700

    优化过滤条件处理，减少总循环次数

--- calculator_of_Onmyoji/cal_and_filter.py
+++ calculator_of_Onmyoji/cal_and_filter.py
@@ -13,51 +13,51 @@ def make_combination(data_dict):
     return list(itertools.product(d1, d2, d3, d4, d5, d6))
 
 
-def filter_by_mitama_type(comb_list, mitama_type, min_num):
+def filter_mitama(mitama_comb_list, mitama_enhance, 
+                  mitama_type='', type_min_num=0, 
+                  prop_type='', prop_min_value=0):
+
+    comb_data_list = cal_mitama_comb_prop(mitama_comb_list, mitama_enhance)
+    
     filter_result = []
-    for comb in comb_list:
-        count = 0
-        for mitama in comb:
-            mitama_info = mitama.values()[0]
-            if mitama_info.get(u'御魂类型') == mitama_type:
-                count += 1
-        if count >= min_num:
-            filter_result.append(comb)
+    for comb_data in comb_data_list:
+        sum_data = comb_data.get('sum', {})
+        if (mitama_type and 
+                not fit_mitama_type(sum_data, mitama_type, type_min_num)):
+            continue
+        if (prop_type and
+                not fit_prop_value(sum_data, prop_type, prop_min_value)):
+            continue
+        filter_result.append(comb_data)
 
     return filter_result
 
 
-def filter_by_prop_value(comb_list, prop_type, min_value, mitama_enhance):
-    filter_result = []
+def fit_mitama_type(comb_sum_data, mitama_type, min_num):
+    mitama_count = comb_sum_data.get(u'御魂计数', {}).get(mitama_type, 0)
+    if mitama_count >= min_num:
+        return True
+    else:
+        return False
+
+
+def fit_prop_value(comb_sum_data, prop_type, min_value):
+    prop_value = comb_sum_data.get(prop_type, 0)
+    if prop_value >= min_value:
+        return True
+    else:
+        return False
+
+
+def cal_mitama_comb_prop(comb_list, mitama_enhance):
+    mitama_comb_data = list()
     for comb in comb_list:
-        mitama_type_count = {}
-        prop_value = 0
-        for mitama in comb:
-            mitama_info = mitama.values()[0]
-            # 指定属性值求和
-            if mitama_info.get(prop_type):
-                prop_value += mitama_info.get(prop_type)
-            # 记录总御魂类型，用于下面计算御魂套装加成 
-            mitama_type = mitama_info.get(u'御魂类型')
-            if mitama_type not in mitama_type_count:
-                mitama_type_count[mitama_type] = 1
-            else:
-                mitama_type_count[mitama_type] += 1
-        for m_type, m_count in mitama_type_count.items():
-            if mitama_enhance[m_type].get(u'加成类型') != prop_type:
-                # 套装效果不属于过滤条件
-                continue
-            if m_count < 2:  # 忽略套装效果
-                continue
-            elif 2 <= m_count <= 5:  # 计算一次套装效果
-                prop_value += mitama_enhance[m_type].get(u'加成数值')
-            elif m_count == 6:  # 计算两次套装效果
-                prop_value += 2 * mitama_enhance[m_type].get(u'加成数值')
+        comb_sum = sum_prop(comb, mitama_enhance)
+        comb_data = {'sum': comb_sum,
+                     'info': comb}
+        mitama_comb_data.append(comb_data)
 
-        if prop_value >= min_value:
-            filter_result.append(comb)
-
-    return filter_result
+    return mitama_comb_data
 
 
 def sum_prop(mitama_comb, mitama_enhance):
@@ -87,6 +87,8 @@ def sum_prop(mitama_comb, mitama_enhance):
             prop_type = mitama_enhance[m_type].get(u'加成类型')
             sum_result[prop_type] += multi_times * mitama_enhance[m_type].get(u'加成数值')
 
+    sum_result[u'御魂计数'] = mitama_type_count
+
     return sum_result
 
 
@@ -96,13 +98,17 @@ if __name__ == '__main__':
     test_file = './example/data_Template.xlsx'
     d = load_data.get_mitama_data(test_file)
     l_d = load_data.sep_mitama_by_loc(d)
+    m_e = load_data.get_mitama_enhance(test_file)
 
     comb = make_combination(l_d)
     print(len(comb))
 
-    f_r = filter_by_mitama_type(comb, u'针女', 4)
-    print(len(f_r))
+    f_t = filter_mitama(comb, m_e, mitama_type=u'针女', type_min_num=4)
+    print(len(f_t))
 
-    m_e = load_data.get_mitama_enhance(test_file)
-    f_p_r = filter_by_prop_value(comb, u'暴击', 90, m_e) 
-    print(len(f_p_r))
+    f_p = filter_mitama(comb, m_e, prop_type=u'暴击', prop_min_value=90) 
+    print(len(f_p))
+
+    f_t_p = filter_mitama(comb, m_e, mitama_type=u'针女', type_min_num=4,
+                          prop_type=u'暴击', prop_min_value=90)
+    print(len(f_t_p))
