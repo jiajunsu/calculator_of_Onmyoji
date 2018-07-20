8d28d90270747daf44516ea290520a0c4398a0b0
chujie <umbrella.j@gmail.com>
2018-07-20 14:42:16 -0400

    Add support for json input file
    Add functions to read json file, and extension name check.

--- calculator_of_Onmyoji/load_data.py
+++ calculator_of_Onmyoji/load_data.py
@@ -2,6 +2,7 @@
 
 import os
 import traceback
+import json
 
 import xlrd
 
@@ -20,8 +21,33 @@ def _get_sheet_rows(filename, sheet_name):
         print(traceback.format_exc())
         raise
 
+def get_mitama_data_json(filename, ignore_serial):
+    def mitama_json_to_dict(json_obj):
+        MITAMA_COL_MAP = {u'御魂序号': u'id', u'御魂类型': u'name', u'位置': u'pos'}
+        serial = json_obj[u'id']
+        if skip_serial(serial, ignore_serial):
+            return None
+        mitama = {}
+        for col_name in data_format.MITAMA_COL_NAME_ZH[1:]:
+            if col_name in MITAMA_COL_MAP:
+                mitama[col_name] = json_obj[MITAMA_COL_MAP[col_name]]
+            else:
+                mitama[col_name] = 0
 
-def get_mitama_data(filename, ignore_serial):
+        for props in [json_obj[u'mainAttr']] + json_obj[u'addiAttr']:
+            mitama[props[u'attrName']] += int(props[u'attrVal'])
+        return (serial, mitama)
+
+    try:
+        with open(filename) as f:
+            data = json.load(f)
+        mitama_list = map(mitama_json_to_dict, data['data'])
+        return dict(filter(lambda x: x, mitama_list))
+    except Exception as e:
+        print("Error loading json file!")
+        return None
+
+def get_mitama_data_xls(filename, ignore_serial):
     rows_data = _get_sheet_rows(filename, sheet_name=u'御魂')
     mitama_data = dict()
     data_len = len(data_format.MITAMA_COL_NAME_ZH)
@@ -42,6 +68,17 @@ def get_mitama_data(filename, ignore_serial):
         mitama_data[serial] = data
 
     return mitama_data
+
+def get_mitama_data(filename, ignore_serial):
+    ext_name = os.path.splitext(filename)[1]
+    if ext_name == '.xls':
+        return get_mitama_data_xls(filename, ignore_serial)
+    elif ext_name == '.json':
+        return get_mitama_data_json(filename, ignore_serial)
+    else:
+        print("Unsupported file type!")
+
+    return dict()
 
 
 def skip_serial(serial, ignore_list):
